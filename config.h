ca90a7634c73ad6d6d46af0dee1227a95aa4c5f5
Federico Ciuffardi <federico.ciuffardi@outlook.com>
2020-08-11 04:30:27 -0300

    applies scrollback

--- config.h
+++ config.h
@@ -172,8 +172,16 @@ static uint forcemousemod = ShiftMask;
  * Internal mouse shortcuts.
  * Beware that overloading Button1 will disable the selection.
  */
+<<<<<<< HEAD
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
+=======
+const unsigned int mousescrollincrement = 2;
+static MouseShortcut mshortcuts[] = {
+	/* mask                 button   function        argument       release */
+	{ XK_ANY_MOD,           Button4, kscrollup,      {.i = mousescrollincrement},      0, /* !alt */ -1 },
+	{ XK_ANY_MOD,           Button5, kscrolldown,    {.i = mousescrollincrement},      0, /* !alt */ -1 },
+>>>>>>> ae5645d... applies scrollback
 	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
 	{ ShiftMask,            Button4, ttysend,        {.s = "\033[5;2~"} },
 	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
@@ -199,6 +207,11 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+<<<<<<< HEAD
+=======
+	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
+	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+>>>>>>> ae5645d... applies scrollback
 };
 
 /*
