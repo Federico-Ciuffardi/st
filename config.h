f9b58a12ec2a286c95c445fa1483f0d8cb19c29e
Federico Ciuffardi <federico.ciuffardi@outlook.com>
2020-11-02 20:23:19 -0300

    sel config change

--- config.h
+++ config.h
@@ -212,7 +212,7 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,               XK_Num_Lock,   numlock,        {.i =  0} },
 	{ TERMMOD,               XK_K,          kscrollup,      {.i = 1} },
 	{ TERMMOD,               XK_J,          kscrolldown,    {.i = 1} },
-	{ TERMMOD,               XK_Escape,     keyboard_select,{.i =  0} },
+	{ TERMMOD,               XK_S,          keyboard_select,{.i =  0} },
 };
 
 /*
