c39967ea99ddac66f4479c81b52629744ae3c345
Federico Ciuffardi <federico.ciuffardi@outlook.com>
2025-05-08 12:05:06 -0300

    Change cursor color

--- config.h
+++ config.h
@@ -131,8 +131,8 @@ static const char *colorname[] = {
 	"#ebdbb2",
 	[255] = 0,
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#add8e6", /* 256 -> cursor */
-	"#555555", /* 257 -> rev cursor*/
+	"#FFFFFF", /* 256 -> cursor */
+	"#FFFFFF", /* 257 -> rev cursor*/
 	"#282828", /* 258 -> bg */
 	"#ebdbb2", /* 259 -> fg */
 };
